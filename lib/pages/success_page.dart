4daa07549d53d912b6243ce4a33550c76375b38c
Gabriel Ferreira <gabrielferrer@outlook.com.br>
2019-09-15 13:18:44 -0300

    success

--- lib/pages/success_page.dart
+++ lib/pages/success_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:hackathonmagalusp/components/custom_drawer.dart';
+import 'package:hackathonmagalusp/pages/home/home_page.dart';
 
 class SuccessPage extends StatefulWidget {
   @override
@@ -20,8 +21,32 @@ class _SuccessPageState extends State<SuccessPage> {
         constraints: BoxConstraints.expand(),
         color: Colors.green,
         child: Center(
-            child: Text('Pedido realizado com sucesso!',
-                style: TextStyle(color: Colors.white))),
+            child: Padding(
+          padding: const EdgeInsets.all(32.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              Text('Pedido realizado com sucesso!',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(color: Colors.white, fontSize: 24)),
+              Padding(
+                padding: const EdgeInsets.symmetric(vertical: 8.0),
+                child: Text('Você receberá um e-mail com os dados da troca',
+                    textAlign: TextAlign.center,
+                    style: TextStyle(color: Colors.white)),
+              ),
+              FlatButton(
+                child: Text('VOLTAR PARA A HOME',
+                    style: TextStyle(color: Colors.white)),
+                onPressed: () {
+                  Navigator.push(context,
+                      MaterialPageRoute(builder: (context) => HomePage()));
+                },
+              )
+            ],
+          ),
+        )),
       ),
     );
   }
